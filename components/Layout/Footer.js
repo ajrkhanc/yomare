bad6943760480a277fff09201f431be4aba4c67d
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-05-30 15:16:16 +0530

    ch

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -17,7 +17,7 @@ export default function Footer() {
 
                     document.getElementById("showlabel").style.display = "block";
                     window.setTimeout(function() {
-                       window.location.href = "/thank-you-lp"
+                    //    window.location.href = "/thank-you-lp"
                     }, 3000);
 
                 } else {
