a0c6a149a4a9b4ec0337cd250352984d980b771f
ajrkhan1 <42898824+ajrkhan1@users.noreply.github.com>
2023-06-08 13:08:17 +0530

    ch

--- pages/thank-you-lp.js
+++ pages/thank-you-lp.js
@@ -5,7 +5,7 @@ export default function ThankU(){
     return(
         <>
          <Head>
-        <title>Thank You LP - Blanchard International</title>
+        <title>Thank You LP - Yoma Business Solutions</title>
         <meta name="description" content="Like us on FACEBOOK @blanchardresearchandtrainingindia Connect with us on LINKEDIN Blanchard Research and Training India Follow us on TWITTER @Blanchard_India A confirmation email will be sent to your email. In case you do not find it in your inbox, check your SPAM folder to access the same."/>
        
         </Head>
